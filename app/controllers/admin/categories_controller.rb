d62b2e1a837e17f5f6e505b1ec6b70b1ff5f3169
Raphael Barbo <rsbarbo@gmail.com>
2016-09-11 19:12:00 -0600

    still need to finish categories

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,6 +1,56 @@
 class Admin::CategoriesController < ApplicationController
+  class CategoriesController < Admin::CategoriesController
+    before_action :require_admin, except: [:index, :show]
 
-  def index
+    def index
+      @categories = Category.all
+    end
+
+    def new
+      @category = Category.new
+    end
+
+    def create
+      @category = Category.new(category_params)
+      if @category.save
+        flash[:notice] = "Category Successfully Created!"
+        redirect_to @category
+      else
+        flash.now[:error] = @category.errors.full_messages.join(", ")
+        render :new
+      end
+    end
+
+    def edit
+      @category = Category.find(params[:id])
+    end
+
+    def update
+      @category = Category.find(params[:id])
+      if @category.update(category_params)
+        flash[:notice] = "Category Successfully Updated!"
+        redirect_to categories_path
+      else
+        flash.now[:error] = @category.errors.full_messages.join(", ")
+        render :edit
+      end
+    end
+
+    def show
+      @admin = current_user.admin?
+      @category = Category.find(params[:id])
+    end
+
+    def destroy
+      flash[:notice] = "Category Successfully Deleted!"
+      category = Category.find(params[:id])
+      category.destroy
+      redirect_to categories_path
+    end
+
+    private
+    def category_params
+      params.require(:category).permit(:name)
+    end
   end
-
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,15 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   def current_user
-      @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def current_admin?
+    current_user && current_user.admin?
+  end
+
+  def require_admin
+    render file: "/public/404" unless current_admin?
   end
 
 end
